4c0041e5c4ad28ad4b988d197c2ec8639db48b83
stefanovic80 <stefanovic80@ucdconnect.ie>
2022-12-09 22:05:46 +0100

    just a couple of c codes!

---
+++ cCode01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int yourInteger;
+	printf("Type here an integer number you want\n");
+	scanf("%d", &yourInteger);
+	printf("\nthe number you typed in is %d\n", yourInteger);
+	return 0;
+}
